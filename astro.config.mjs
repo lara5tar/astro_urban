27555731bb4b77fcf162fbf7de71f885a1fd9163
Tu nombre de usuario <analaramtzhdz@gmail.com>
2025-03-02 15:21:20 -0600

    Add Tailwind CSS integration and create SocialIconsRow component; update Navbar links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+
+    },
+});
